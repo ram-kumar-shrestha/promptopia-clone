cc79cc1aacb66e0ee86f056a27bfe1e8e5031e3d
ram <stharamkumar3303@gmail.com>
2023-12-05 08:06:08 +0545

    oauth implemented

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -8,10 +10,52 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-    } catch (error) {}
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if user alredy exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // Generate a username based on the email
+        const username = profile.email.split("@")[0];
+
+        // Ensure the username is between 8-20 characters
+        const truncatedUsername = username.slice(0, 20);
+
+        // Check if the generated username is unique
+        const uniqueUsername = await User.findOne({
+          username: truncatedUsername,
+        });
+
+        // If not unique, append a random string to ensure uniqueness
+        const finalUsername = uniqueUsername
+          ? `${truncatedUsername}-${Math.random().toString(36).substring(2, 8)}`
+          : truncatedUsername;
+
+        //new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: finalUsername,
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,24 +1,24 @@
 "use client";
 
-import { getProviders, signIn, signOut } from "next-auth/react";
+import { getProviders, signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [toggleDropdown, setToggleDropdown] = useState(false);
   const [providers, setProviders] = useState(null);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -36,7 +36,7 @@ const Nav = () => {
 
       {/* desktop navigation */}
       <div className="sm:flex hidden ">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href={"/create-prompt"} className="black_btn">
               Create Post
@@ -48,7 +48,7 @@ const Nav = () => {
 
             <Link href={"/profile"}>
               <Image
-                src={"/assets/images/logo.svg"}
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -75,10 +75,10 @@ const Nav = () => {
 
       {/*  Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src={"/assets/images/logo.svg"}
+              src={session?.user?.image}
               width={37}
               height={37}
               className="rounded-full"
